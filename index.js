f5bb720c7a6c7229ac512ea77fce695719a5ca29
Rohit RRJ <rrajhans04@gmail.com>
2025-07-21 16:02:39 +0530

    modifying our index.js in 2nd lecture

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/babu', (req, res) => {
   res.send('Hello World! fuk off')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send("<h2>this is you tube</h2>")
 })
 
 app.listen(process.env.PORT, () => {
